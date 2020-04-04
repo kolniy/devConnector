bd856e3f2e228a60dea530e443a44629fb645fd0
kolawole olaniyi <kolaniyi3@gmail.com>
2020-04-04 16:17:14 +0100

    commit init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API running')
+})
+
+const PORT = process.env.PORT || 3000
+ 
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+
+
+// mongodb password = kolniy1234
