1109608ba0aef28aacbfc2117da9836e376818df
kolawole olaniyi <kolaniyi3@gmail.com>
2020-08-28 17:29:51 +0100

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDb = require("./config/connection");
+const path = require("path")
 
 const app = express();
 
@@ -9,9 +10,6 @@ connectDb();
 // Initialise Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -19,12 +17,17 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/posts"));
 
+
+if(process.env.NODE_ENV === 'production'){
+    // set static files
+    app.use(express.static('client/build'))
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
 
-// things to google
-// how to get connection string for my local database for development
-// how to use env cmd
-
-// / "mongoURI": "mongodb+srv://kolniy:kolniy1234@developershangout-kv8uf.mongodb.net/test?retryWrites=true&w=majority",
