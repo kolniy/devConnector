7846cd73f2e83ffd47ddbf6bd230eaaeae702dfe
kolniy <kolaniyi3@gmail.com>
2022-01-23 00:03:37 +0100

    general update

--- client/src/components/layouts/NotFound.js
+++ client/src/components/layouts/NotFound.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function NotFound(props) {
     return (
-        <div>
-            Page not found
-        </div>
+        <>
+            <h1 className="x-large text-primary">
+            <i className="fa fa-exclamation-triang"></i> Page Not Found.
+            </h1>
+            <p className="large">Sorry, This page does not Exist's</p>
+        </>
     )
 }
 
